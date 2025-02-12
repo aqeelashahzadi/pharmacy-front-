9c472b59486bb3d0dc90a340a4533629c48c1332
aqeelashahzadi <aqeelaghafooroff@gmail.com>
2025-02-11 23:07:03 -0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const mobile_nav = document.querySelector(".res-icon");
-const nav_header = document.querySelector(".forresponsiveness");
-
-const toggleNavbar = () => {
-  nav_header.classList.toggle("start");
-};
-
-mobile_nav.addEventListener("click", () => toggleNavbar());
